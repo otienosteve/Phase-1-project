db2393d0a9bcfb00b947d08abec82263d2482ed8
otienosteve <steveotieno701@gmail.com>
2022-09-19 18:50:08 +0300

    Add Speech Synthesis

--- script.js
+++ script.js
@@ -20,15 +20,35 @@ fetch('https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand').then(res=>
 .then(data=>{
     for ( datum of data){
         let div =document.createElement('div')
+        div.classList.add('quote')
         div.innerHTML=datum.content.rendered
         document.querySelector('.quotes-display').append(div)
     }
 
-
+    let quotes=document.querySelectorAll('.quote')
+    // console.log(quotes)
+    quotes.forEach(elem=>{
+        console.log(elem)
+        
+        elem.addEventListener('click',()=>{        
+        speak(elem.innerText)})
+    
+    
+    })
 
 
 })
 
 
+function speak( text ){
+
+    let say = new SpeechSynthesisUtterance(text);
+    say.pitch = 3;               // Set Pitch 
+    say.rate = 2;                // Set Rate (speed)
+    say.lang = "en-US";          // Set Language
+    speechSynthesis.speak(say);  // Speak
+  
+  }
+  speak()
 
 })
